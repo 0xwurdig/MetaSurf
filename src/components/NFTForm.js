688602a1cec6286a1a079b42ed911ff779b5854a
aeesh <adamaisha352@gmail.com>
2022-03-26 16:00:07 +0100

    clean

--- src/components/NFTForm.js
+++ src/components/NFTForm.js
@@ -8,10 +8,6 @@ const NFTForm = () => {
   const [value, setValue] = useState("");
   const [description, setDescription] = useState("");
 
-  console.log('name', name);
-  console.log('value', value);
-  console.log('rewardType', rewardType);
-
   return (
     <div className="w-full">
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
